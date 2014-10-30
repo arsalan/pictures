19ce5fcdb648e5c617f555b2e20cedcbd43dc431
Arsalan Ahmed <itsarsalan@yahoo.com>
2014-10-29 21:18:27 -0400

    added model v1

---
+++ PictureAlbum/Models/PictureAlbumV1.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ImageOrganizer.Models
+{
+    [JsonObject(Title = "pictureAlbum")]
+    public class PictureAlbumV1
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
